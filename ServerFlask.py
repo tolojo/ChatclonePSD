7b54ca61fa6bde27fefe5867b20967fb04394294
tolojo <tomasferreira91@gmail.com>
2022-11-17 15:02:04 +0000

    Já recebemos a key e guardamos num dicionário local e devolvemos a lista de keys guardadas
    Falta geração da key

--- ServerFlask.py
+++ ServerFlask.py
@@ -10,29 +10,32 @@ db = client.get_database("userDatabase")
 users = db.User
 
 
-@app.route('/users', methods=['POST'])
-def list_programming_languages():
+@app.route('/users', methods=['POST']) #Login do user
+def userAuth():
     data = json.dumps(request.get_json())
     data = json.loads(data)
     print(data)
     login = users.find_one({"uname": data["uname"], "passwd": data["passwd"]})
-    if (login):
+    if login:
         return "User encontrado"
     else:
         return "User não encontrado"
+
 
 @app.route('/users/pkRegister', methods=['POST']) #registar a PK do user no server
-def list_programming_languages():
+def pkRegister():
     data = json.dumps(request.get_json())
     data = json.loads(data)
     print(data)
-    login = users.find_one({"uname": data["uname"], "passwd": data["passwd"]})
-    if (login):
-        return "User encontrado"
-    else:
-        return "User não encontrado"
+    UserPK_pair[data["uname"]] = "PK teste1" # sub pk teste1 por futura PK
+    print(UserPK_pair)
+    return list(UserPK_pair.items())
 
 
+
+@app.route('/users/pkRegister', methods=['POST']) #registar a PK do user no server
+def pkRetrieve():
+    return UserPK_pair
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=3000)
